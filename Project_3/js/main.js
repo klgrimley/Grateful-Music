450b80a3fec4924ab9ce1681241b2629ae6e7ff9
Gabriel Sundberg <chewy4ever31@yahoo.com>
2014-04-24 11:23:47 -0400

    4/24

--- Project_3/js/main.js
+++ Project_3/js/main.js
@@ -1,16 +1,10 @@
 // JavaScript Document
 //runs javascript as soon as the page loads
-	var albums;
-	var albumInfoTemplate = Handlebars.compile($("#albumInfo").html());
 		$(document).ready(function(){
-			var contentDiv = $("#content");
+			$('.sub').hide();
 
 			//when sub is clicked exposes the list
 			$("#artist").click(function(){
 				$("#artists").toggle('fast');
-			});
-
-			$.getJSON('json/data.json', function(data) {
-				$("#content").append
 			});
 		});
